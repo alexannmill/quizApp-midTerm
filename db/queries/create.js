7f86f0571c00e17a0d56deaab639df55bcc6f566
alexannmill <alexannmill@gmail.com>
2022-09-26 02:42:15 +0000

    client side js started for name entry completed query func

--- db/queries/create.js
+++ db/queries/create.js
@@ -1,18 +1,30 @@
 const db = require('../connection')
 
+//generate shortURL link
+const generateRandomNumber = () => {
+  return Math.random().toString(36).substring(2, 8);
+};
+exports.generateRandomNumber = generateRandomNumber
 
-// allq's need to be made before quiz (FK)
-const createQuestion = () => {
-  return db.query(``)
+// all's need to be made before quiz (FK)
+const createQuestion = (question) => {
+  return db.query(`
+  INSERT INTO TABLE questions
+  (question, correct, answer1, answer2, answer3, answer4)
+  VALUES
+  ($1, $2, $3, $4, $5, $6)`
+  ,[])
   .then()
 };
 exports.createQuestion = createQuestion
 
 //
-const createQuiz = () => {
-  return db.query(``)
+const createQuiz = (name, shortURL) => {
+  return db.query(`INSERT INTO TABLE quizzes
+  (name, short_url, user_id, question1, question2, question3)
+  VALUES
+  ($1, $2, $3, $4, $5)`
+  ,[name, shortURL,null, null, null])
   .then()
 }
 exports.createQuiz = createQuiz
-
-
--- routes/create.js
+++ routes/create.js
@@ -10,15 +10,83 @@ router.get('/', (req, res) => {
   res.render('create');
 });
 
-//final page to send to db
-//GET /create/:id
-router.get('/:id', (res, req) => {
-  db.query (``)
-  .then(() => {
+//func for dropping name entry and appending questions
+const startQuiz = $(() => {
+  const $main = $('#main');
+  // HTML for questions form
+  const $question = $(`  <section class="questionContainer">
+      <form action="/routes/create.js" method="post">
+        <textarea name="question" id="" cols="30" rows="10"></textarea>
+        <button type="submit">Start/button</button>
+       </form>
+    </section>
+    <section id="answerContainer">
+      <form class="anwsertext" action="/routes/create.js" method="post">
+        <textarea name="correct" id="" cols="30" rows="10"></textarea>
+       </form>
+      <form class="anwsertext" action="/routes/create.js" method="post">
+        <textarea name="answer1" id="" cols="30" rows="10"></textarea>
+       </form>
+      <form class="anwsertext" action="/routes/create.js" method="post">
+        <textarea name="answer2" id="" cols="30" rows="10"></textarea>
+       </form>
+      <form class="anwsertext" action="/routes/create.js" method="post">
+        <textarea name="answer3" id="" cols="30" rows="10"></textarea>
+       </form>
+      <form class="anwsertext" action="/routes/create.js" method="post">
+        <textarea name="answer4" id="" ></textarea>
+        <button type="submit">Start/button</button>
+       </form>
+    </section>`)
 
-  })
-});
+//dropping name
+  $createContainer.detach();
+//appending questions
+  $question.append($main);
+})
+//adding to data on submit and pulling up questions after name submission
+($('.quizName').submit(function (e) {
+  e.preventDefault();
+  const name = $(this).serialize();
+  const shortURL = db.generateRandomNumber;
+  db.createQuiz(name, shortURL);
+  startQuiz();
+})
+)
 
+
+// //name entered redirect to questions form
+// router.post('/', (req, res) => {
+//   // if (!req.body.text) {
+//   //   alert("Plz enter a name");
+//   //   return;
+//   // }
+//   console.log('req.body:', req.body)
+//   const name = req.body;
+//   const shortUrl = db.generateRandomNumber();
+//   console.log('shortUrl:', shortUrl)
+//   console.log('req.body:', req.body)
+//  res.redirect(`/create/${shortUrl}`);
+// });
+
+// //final page to send to db
+// //GET /create/:id
+// router.get('/:url', (req, res) => {
+//   console.log('req.params:', req.params)
+//   const url = req.params.name
+//   res.render('questions');
+// });
+
+// //form for each question
+// router.post('/:url', (res, req) => {
+//   db.query (``)
+//   .then(() => {
+
+//   })
+// });
+
+
+//   //questions append
 
 
 
