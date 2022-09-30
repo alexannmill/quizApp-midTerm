10aeff845ba5e7762593d4cbf609b830b804a97a
alexannmill <alexannmill@gmail.com>
2022-09-30 06:24:17 +0000

    render edits for answer and wuestion for mistake

--- public/scripts/questions.js
+++ public/scripts/questions.js
@@ -4,41 +4,37 @@ $(function () {
   $("#completeQuizCreate").hide();
 
   //submit for question form
-  $("#questionform").submit(function(e) {
-  e.preventDefault();
-  const quiz_id = $("#questionform").attr("data-quiz-id");
-//collecting question data
-  const question = {};
-  question.question = $(this).find('textarea[name="question"]').val();
-  question.correct = $(this).find('textarea[name="correct"]').val();
-  question.answer1 = $(this).find('textarea[name="answer1"]').val();
-  question.answer2 = $(this).find('textarea[name="answer2"]').val();
-  question.answer3 = $(this).find('textarea[name="answer3"]').val();
-  question.answer4 = question.correct
+  $("#questionform").submit(function (e) {
+    e.preventDefault();
+    const quiz_id = $("#questionform").attr("data-quiz-id");
+    //collecting question data
+    const question = {};
+    question.question = $(this).find('textarea[name="question"]').val();
+    question.correct = $(this).find('textarea[name="correct"]').val();
+    question.answer1 = $(this).find('textarea[name="answer1"]').val();
+    question.answer2 = $(this).find('textarea[name="answer2"]').val();
+    question.answer3 = $(this).find('textarea[name="answer3"]').val();
+    question.answer4 = question.correct;
 
-//error handling for incorrect input
-  if (!question.question || !question.correct) {
-    if (
-      !question.answer1 ||
-      !question.answer2 ||
-      !question.answer3
-    ) {
-      alert("Plz enter a question and at least 2 answers");
-      return;
+    //error handling for incorrect input
+    if (!question.question || !question.correct) {
+      if (!question.answer1 || !question.answer2 || !question.answer3) {
+        alert("Plz enter a question and at least 2 answers");
+        return;
+      }
     }
-  }
- //sending questions
-  $.post(`/create/${quiz_id}`, question, function(data) {
-    const numOFq = data.numOFq;
-//for generate quiz button
-    if (numOFq > 2) {
-      $("#completeQuizCreate").show();
-    }
-    $("#questionform").trigger("reset");
+    //sending questions
+    $.post(`/create/${quiz_id}`, question, function (data) {
+      const numOFq = data.numOFq;
+      //for generate quiz button
+      if (numOFq > 2) {
+        $("#completeQuizCreate").show();
+      }
+      $("#questionform").trigger("reset");
+    });
   });
-});
 
-//func for dropping qNa text form for final page
+  //func for dropping qNa text form for final page
   const HTMLswapFinal = (quiz_id) => {
     const $finalHTML = `<section id="createComplete">
     <h1> Your Quiz is Complete!</h1>
@@ -60,29 +56,29 @@ $(function () {
     $("#completeQuizCreate").detach();
     $("#main").append($finalHTML);
   };
-//submit generate quiz
-  $("#completeQuizCreate").submit(function(e) {
+  //submit generate quiz
+  $("#completeQuizCreate").submit(function (e) {
     e.preventDefault();
     const quiz_id = $("#completeQuizCreate").attr("data-quiz-id");
     HTMLswapFinal(quiz_id);
   });
-//make public quiz changes status in db
+  //make public quiz changes status in db
   $("#makePublic").submit(function (e) {
     e.preventDefault();
     const quiz_id = $("#completeQuizCreate").attr("data-quiz-id");
-    $.post(`/create/${quiz_id}/public`)
+    $.post(`/create/${quiz_id}/public`);
   });
-//overview of created quiz
-  $("#overview").submit(function(e) {
+  //overview of created quiz
+  $("#overview").submit(function (e) {
     e.preventDefault();
     const quiz_id = $("#completeQuizCreate").attr("data-quiz-id");
     $.post(`/create/${quiz_id}/complete`);
   });
 
-//redirect for creating another quiz
+  //redirect for creating another quiz
   $("#createComplete").submit(function (e) {
     $.get(`/create/`);
   });
 
-    //HTML for final create page
+  //HTML for final create page
 });
